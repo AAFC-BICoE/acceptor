6b3b3e205448c81b41d32a452bf3317c3574bc75
glen newton <glen.newton@gmail.com>
2014-07-07 19:41:35 -0400

    Support multi-line message

--- accept_message.sh
+++ accept_message.sh
@@ -19,8 +19,14 @@
 #           - must be world writable
 #
 
+# Any number of lines:
+message[0]="[example] Foobar varsion 1.7 installed; Foobar v.1.6 still available at /opt/old/foobar"
+message[1]="   Foobar v.1.6 still available at /opt/old/foobar"
+message[2]="   Any issues please contact foo@bar.com"
 
-message="[example] Foobar varsion 1.7 installed; Foobar v.1.6 still available at /opt/old/foobar mm"
+# Alternate form acceptable:
+#  message=("[example] Foobar varsion 1.7 installed; Foobar v.1.6 still available at /opt/old/foobar", "   Foobar v.1.6 still available at /opt/old/foobar", " Any issues please contact foo@bar.com")
+
 #
 prompt="Please acknowledge you have read and understand the above message by typing \"yes\" at this prompt: "
 
@@ -30,6 +36,24 @@ logfile="/var/log/accepted_message.log"
 ###### DO NOT CHANGE BELOW THIS POINT ###########
 #################################################
 
+
+print_message(){
+    printf "%s\n" "$@"
+ }
+
+message_key(){
+    key=""
+    for i in "${@}"
+    do
+	key+=$i
+	key+=" "
+    done
+    echo "$key"
+
+
+ }
+
+messageKey=$(message_key "${message[@]}")
 
 if [ ! -f $logfile ]; then
     echo ""
@@ -44,10 +68,11 @@ else
 	    ##
 
 	    echo ""
-	    echo $message
+	    #print_message ${message[@]}"
+	    print_message "${message[@]}" # works!
 	    echo ""
 
-	    userMessageKey="$USER|$message"
+	    userMessageKey="$USER|$messageKey"
 
 	    #hits=`grep -F -c "$userMessageKey\\$" $logfile`
 	    hits=`grep -F -c "$userMessageKey" $logfile`
